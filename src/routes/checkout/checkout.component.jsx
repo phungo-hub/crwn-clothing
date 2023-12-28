f845af0f13d32a56fe0d621f542d956e524eb90f
phungo-hub <ngokieuphu@gmail.com>
2023-12-28 17:16:26 -0500

    add payment function with Stripe and update Netlify functions

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
 import { selectCartItems, selectCartTotal } from "../../store/cart/cart.selector";
 
+import PaymentForm from "../../components/payment-form/payment-form.component";
+
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
 
 import "./checkout.styles.scss";
@@ -31,6 +33,7 @@ const Checkout = () => {
         <CheckoutItem key={cartItem.id} cartItem={cartItem} />
       ))}
       <div className="total">Total: ${cartTotal}</div>
+      <PaymentForm />
     </div>
   );
 };
